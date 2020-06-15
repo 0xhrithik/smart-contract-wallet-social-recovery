e73e9c8313d16782528d1b537afa3fcc7bd811d9
hrithiklodha <hrithikjain909@gmail.com>
2020-06-15 23:59:35 +0530

    add update friend func

--- packages/buidler/contracts/SmartContractWallet.sol
+++ packages/buidler/contracts/SmartContractWallet.sol
@@ -5,6 +5,9 @@ import "@nomiclabs/buidler/console.sol";
 contract SmartContractWallet {
 
   address public owner;
+  mapping(address => bool) public friends;
+
+  event UpdateFriend(address sender, address friend, bool isFriend);
 
   constructor(address _owner) public {
     owner = _owner;
@@ -15,6 +18,28 @@ contract SmartContractWallet {
     require(msg.sender==owner,"NOT THE OWNER!");
     console.log(msg.sender,"withdraws",(address(this)).balance);
     msg.sender.transfer((address(this)).balance);
+  }
+
+  function isOwner(address possibleOwner) public view returns (bool) {
+    return (possibleOwner==owner);
+  }
+
+  function updateOwner(address newOwner) public {
+    require(isOwner(msg.sender),"NOT THE OWNER!");
+    owner = newOwner;
+  }
+
+  function updateFriend(address friendAddress, bool isFriend) public {
+  require(isOwner(msg.sender),"NOT THE OWNER!");
+  friends[friendAddress] = isFriend;
+  console.log(friendAddress,"friend bool set to",isFriend);
+  emit UpdateFriend(msg.sender,friendAddress,isFriend);
+}
+
+  uint constant public limit = 0.005 * 10**18;
+  fallback() external payable {
+    require(((address(this)).balance) <= limit, "WALLET LIMIT REACHED");
+    console.log(msg.sender,"just deposited",msg.value);
   }
 
 }
--- packages/react-app/src/SmartContractWallet.js
+++ packages/react-app/src/SmartContractWallet.js
@@ -25,10 +25,38 @@ export default function SmartContractWallet(props) {
 
   const contractAddress = readContracts?readContracts[contractName].address:""
   const contractBalance = useBalance(contractAddress,props.localProvider)
+  
+  const [ friendAddress, setFriendAddress ] = useState("")
 
+  const updateFriend = (isFriend)=>{
+    return ()=>{
+      tx(writeContracts['SmartContractWallet'].updateFriend(friendAddress, isFriend))
+      setFriendAddress("")
+    }
+  }
+
+  let ownerDisplay = []
+  if(props.address===owner){
+    ownerDisplay.push(
+      <Row align="middle" gutter={4}>
+        <Col span={8} style={{textAlign:"right",opacity:0.333,paddingRight:6,fontSize:24}}>Friend:</Col>
+        <Col span={10}>
+          <AddressInput
+            value={friendAddress}
+            ensProvider={props.ensProvider}
+            onChange={(address)=>{setFriendAddress(address)}}
+          />
+        </Col>
+        <Col span={6}>
+          <Button style={{marginLeft:4}} onClick={updateFriend(true)} shape="circle" icon={<CheckCircleOutlined />} />
+          <Button style={{marginLeft:4}} onClick={updateFriend(false)} shape="circle" icon={<CloseCircleOutlined />} />
+        </Col>
+      </Row>
+    )
+  }
 
   let cardActions = []
-  if(props.address==owner){
+  if(props.address===owner){
     cardActions = [
       <div onClick={()=>{
         tx(writeContracts['SmartContractWallet'].withdraw())
@@ -68,7 +96,7 @@ export default function SmartContractWallet(props) {
             }}/>
           </Col>
         </Row>
-        
+       {ownerDisplay} 
       </div>
     )
   }
@@ -100,7 +128,20 @@ export default function SmartContractWallet(props) {
             )}
           />
       </Card>
-
+      <List
+        style={{ width: 550, marginTop: 25}}
+        header={<div><b>Friend Updates</b></div>}
+        bordered
+        dataSource={friendUpdates}
+        renderItem={item => (
+          <List.Item style={{ fontSize:22 }}>
+            <Address 
+              ensProvider={props.ensProvider} 
+              value={item.friend}
+            /> {item.isFriend?"✅":"❌"}
+          </List.Item>
+        )}
+      />
       <div style={{position:'fixed',textAlign:'right',right:25,top:90,padding:10,width:"50%"}}>
         <h1><span role="img" aria-label="checkmark">✅</span> TODO LIST</h1>
         <Timeline
